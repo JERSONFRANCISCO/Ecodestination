aa9d96b77886b49b1a8adf1c1dc7b8a5c5e962a8
JERSONHIDALGO <jersonfjl@live.com>
2019-02-03 11:51:19 -0600

    identar

--- app_core/models/mdl_tours.php
+++ app_core/models/mdl_tours.php
@@ -1,18 +1,18 @@
 <?php
-    require_once 'mdl_conexion.php';
+require_once 'mdl_conexion.php';
 
 
-   class mdl_tours{
+class mdl_tours{
 
-   	private $conexion;
+	private $conexion;
 
-   	public function __construct(){
-			$this->conexion = new mdl_Conexion();	   
+	public function __construct(){
+		$this->conexion = new mdl_Conexion();	   
 	} 	
-  	  	public function obtener_tours($nombre){
+	public function obtener_tours($id){
 		$this->conexion->consulta("SELECT tdestinos.nombre,tbtours.idtbl_tours,tbtours.titulo,tbtours.descripcion,tbtours.img 
 			from tbl_destinos tdestinos , tbl_tours_has_destinos thdestinos , tbl_tours tbtours
-			where tdestinos.idtbl_destinos = '".$nombre."'
+			where tdestinos.idtbl_destinos = '".$id."'
 			and tdestinos.idtbl_destinos = thdestinos.tbl_destinos_idtbl_destinos
 			and thdestinos.tbl_tours_idtbl_tours = tbtours.idtbl_tours");
 		$posts=array();
@@ -27,119 +27,119 @@
 		    $cont++;
 		}
 		return $posts;
-  	}
+	}
 
-  	  	  	public function insertarTours($titulo,$descripcion,$img,$destino){
+	public function insertarTours($titulo,$descripcion,$img,$destino){
 
-  	  	  		$this->conexion->consulta("SELECT idtbl_tours, titulo, descripcion, img FROM tbl_tours WHERE titulo = '".$titulo."'" );
+		$this->conexion->consulta("SELECT idtbl_tours, titulo, descripcion, img FROM tbl_tours WHERE titulo = '".$titulo."'" );
 
-  					$posts=array();
-  					$cont=0;
+		$posts=array();
+		$cont=0;
 
-  					if ($this->conexion->extraer_registro()) {
+		if ($this->conexion->extraer_registro()) {
 
-  						$this->conexion->consulta("SELECT idtbl_tours FROM tbl_tours order by idtbl_tours desc limit 1");
-  						$a="";
-  	  						while ($fila = $this->conexion->extraer_registro()) {
+			$this->conexion->consulta("SELECT idtbl_tours FROM tbl_tours order by idtbl_tours desc limit 1");
+			$a="";
+			while ($fila = $this->conexion->extraer_registro()) {
 		    					$a = $fila[0]; //p_nombre
 		    				}
 
-								$this->conexion->consulta("SELECT id_tbl_tours_has_destinos, tbl_tours_idtbl_tours, tbl_destinos_idtbl_destinos FROM tbl_tours_has_destinos WHERE tbl_tours_idtbl_tours=".$a." && tbl_destinos_idtbl_destinos= ".$destino." " );
+		    				$this->conexion->consulta("SELECT id_tbl_tours_has_destinos, tbl_tours_idtbl_tours, tbl_destinos_idtbl_destinos FROM tbl_tours_has_destinos WHERE tbl_tours_idtbl_tours=".$a." && tbl_destinos_idtbl_destinos= ".$destino." " );
 
-								if ($this->conexion->extraer_registro()) {
+		    				if ($this->conexion->extraer_registro()) {
 
-		    						echo "<script>alert('ya existe este tour!!')</script>";  
+		    					echo "<script>alert('ya existe este tour!!')</script>";  
 
-		    						}else{
-		     	 						$this->conexion->consulta("INSERT INTO tbl_tours_has_destinos(tbl_tours_idtbl_tours, tbl_destinos_idtbl_destinos) VALUES (".$a.",".$destino.")");
-		     	 						echo "<script>alert('ingreso tbl_tours_has_destinos!!')</script>";  
-		     	 					}
+		    				}else{
+		    					$this->conexion->consulta("INSERT INTO tbl_tours_has_destinos(tbl_tours_idtbl_tours, tbl_destinos_idtbl_destinos) VALUES (".$a.",".$destino.")");
+		    					echo "<script>alert('ingreso tbl_tours_has_destinos!!')</script>";  
+		    				}
 
-  					}else{
-  						$this->conexion->consulta("INSERT INTO tbl_tours(titulo, descripcion, img) VALUES ('".$titulo."','".$descripcion."','".$img."')");
-  			
-  						$this->insertarTourshasDestino($destino);
-  			
-					}
-  			
-				}
-  	  	
-			public function insertarTourshasDestino($destino){
+		    			}else{
+		    				$this->conexion->consulta("INSERT INTO tbl_tours(titulo, descripcion, img) VALUES ('".$titulo."','".$descripcion."','".$img."')");
+		    				
+		    				$this->insertarTourshasDestino($destino);
+		    				
+		    			}
+		    			
+		    		}
+		    		
+		    		public function insertarTourshasDestino($destino){
 
-			$this->conexion->consulta("SELECT idtbl_tours FROM tbl_tours order by idtbl_tours desc limit 1");
-  			$a="";
-  	  		while ($fila = $this->conexion->extraer_registro()) {
+		    			$this->conexion->consulta("SELECT idtbl_tours FROM tbl_tours order by idtbl_tours desc limit 1");
+		    			$a="";
+		    			while ($fila = $this->conexion->extraer_registro()) {
 		    $a = $fila[0]; //p_nombre
 
-			}
-		    $this->conexion->consulta("INSERT INTO tbl_tours_has_destinos(tbl_tours_idtbl_tours, tbl_destinos_idtbl_destinos) VALUES (".$a.",".$destino.")");
+		}
+		$this->conexion->consulta("INSERT INTO tbl_tours_has_destinos(tbl_tours_idtbl_tours, tbl_destinos_idtbl_destinos) VALUES (".$a.",".$destino.")");
 
-			}
+	}
 
-			public function insertarItinerario($descrip,$titu,$dia){
+	public function insertarItinerario($descrip,$titu,$dia){
 
-			$this->conexion->consulta("SELECT idtbl_tours FROM tbl_tours order by idtbl_tours desc limit 1");
-  			$a="";
-  	  		while ($fila = $this->conexion->extraer_registro()) {
+		$this->conexion->consulta("SELECT idtbl_tours FROM tbl_tours order by idtbl_tours desc limit 1");
+		$a="";
+		while ($fila = $this->conexion->extraer_registro()) {
 		    $a = $fila[0]; //p_nombre
 
-			}
+		}
 
-			$this->conexion->consulta("INSERT INTO tbl_itinerario( tbl_tours_idtbl_tours, descripcion, titulo, dia) VALUES (".$a.",'".$descrip."','".$titu."',".$dia.")");
-			}
-			
+		$this->conexion->consulta("INSERT INTO tbl_itinerario( tbl_tours_idtbl_tours, descripcion, titulo, dia) VALUES (".$a.",'".$descrip."','".$titu."',".$dia.")");
+	}
+	
 
 
-			public function obtener_tours_combo($destino){
+	public function obtener_tours_combo($destino){
 
-				$this->conexion->consulta("SELECT tbl_tours.idtbl_tours, tbl_tours.titulo FROM tbl_tours , tbl_tours_has_destinos WHERE tbl_tours.idtbl_tours = tbl_tours_has_destinos.tbl_tours_idtbl_tours && tbl_tours_has_destinos.tbl_destinos_idtbl_destinos = ".$destino." " );
+		$this->conexion->consulta("SELECT tbl_tours.idtbl_tours, tbl_tours.titulo FROM tbl_tours , tbl_tours_has_destinos WHERE tbl_tours.idtbl_tours = tbl_tours_has_destinos.tbl_tours_idtbl_tours && tbl_tours_has_destinos.tbl_destinos_idtbl_destinos = ".$destino." " );
 
-				$caja_reporte;
-				$caja_reporte = "<select name='turismo' id='turismo'  onchange='cargarTourAjax(); cargarComboItinerario();' >
-				 <option value=0> Seleccione un Tour</option>" ;
+		$caja_reporte;
+		$caja_reporte = "<select name='turismo' id='turismo'  onchange='cargarTourAjax(); cargarComboItinerario();' >
+		<option value=0> Seleccione un Tour</option>" ;
 
-				while ($fila = $this->conexion->extraer_registro()) {
+		while ($fila = $this->conexion->extraer_registro()) {
 
-		    					$caja_reporte.= "
-                                  <option value=".$fila[0]."> ".$fila[1]."</option>
-                                  ";
-                  }
-							
-                                   $caja_reporte.=" </select> <br>";
-                                
+			$caja_reporte.= "
+			<option value=".$fila[0]."> ".$fila[1]."</option>
+			";
+		}
+		
+		$caja_reporte.=" </select> <br>";
+		
 								  //echo "<script>alert(".$caja_reporte.");</script>"; 
-                                echo $caja_reporte;
+		echo $caja_reporte;
 
-		    	}
+	}
 
-		    public function	mostrar_tours($tourismo){
+	public function	mostrar_tours($tourismo){
 
-		    	$this->conexion->consulta("SELECT idtbl_tours, titulo, descripcion, img FROM tbl_tours WHERE idtbl_tours = ".$tourismo."" );
-		    	
+		$this->conexion->consulta("SELECT idtbl_tours, titulo, descripcion, img FROM tbl_tours WHERE idtbl_tours = ".$tourismo."" );
+		
 
-		    	$caja_reporte;
+		$caja_reporte;
 
-			while ($fila = $this->conexion->extraer_registro()) {
+		while ($fila = $this->conexion->extraer_registro()) {
 
-		          $caja_reporte =" 
-		          				  <form style='color: black;' enctype='multipart/form-data' method='post'>
-		          				  <h2 style='color:black;'>Editar Destino</h2>
-		          				  <img src='app_core/resources/dias/".$fila[3]."' id='img' style='height: 200px; width: 300px;' ><br>
-		          				  <br>
-		          				  <input type='file' name='fotografia' id='fotografia' required tabindex='2'onchange='mostrar3()'/><br>
-		          				  Nombre Destino:<br>
-		          				  <input type='text' name='txt_destino' id='txt_destino' value='".$fila[1]."' class='btn btn-outline-dark' style=' width: 300px;' required><br>
-		          				  Detalle:<br>
-		          				  <textarea rows='4' cols='22' name='txt_descripcion' id='txt_descripcion' class='btn btn-outline-dark' style=' width: 300px;' required> ".$fila[2]."
-		          				  </textarea><br>
-		          		     	  <input type='submit' id='btn_guardarInfo' value='Guardar Info' name='btn_guardarInfo' class='btn btn-outline-dark'><i class='fa fa-sign-out'></i> 
-		          				</form>
-                          ";
-			} 
+			$caja_reporte =" 
+			<form style='color: black;' enctype='multipart/form-data' method='post'>
+			<h2 style='color:black;'>Editar Destino</h2>
+			<img src='app_core/resources/dias/".$fila[3]."' id='img' style='height: 200px; width: 300px;' ><br>
+			<br>
+			<input type='file' name='fotografia' id='fotografia' required tabindex='2'onchange='mostrar3()'/><br>
+			Nombre Destino:<br>
+			<input type='text' name='txt_destino' id='txt_destino' value='".$fila[1]."' class='btn btn-outline-dark' style=' width: 300px;' required><br>
+			Detalle:<br>
+			<textarea rows='4' cols='22' name='txt_descripcion' id='txt_descripcion' class='btn btn-outline-dark' style=' width: 300px;' required> ".$fila[2]."
+			</textarea><br>
+			<input type='submit' id='btn_guardarInfo' value='Guardar Info' name='btn_guardarInfo' class='btn btn-outline-dark'><i class='fa fa-sign-out'></i> 
+			</form>
+			";
+		} 
 
-			  echo $caja_reporte;
+		echo $caja_reporte;
 
-		 }
-			
- }
+	}
+	
+}
 ?>
